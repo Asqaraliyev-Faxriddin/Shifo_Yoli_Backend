a6ba0af811e243f9d1425ab13ee74b8a82b26e9c
hacerovhacer <hacerovhacer253@gmail.com>
2025-09-24 16:06:08 +0500

    g

--- src/modules/auth/stratagies/github.strategy.ts
+++ src/modules/auth/stratagies/github.strategy.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Strategy, VerifyCallback } from 'passport-github2';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -9,33 +9,26 @@ dotenv.config();
 export class GithubStrategy extends PassportStrategy(Strategy, 'github') {
   constructor() {
     super({
+
       clientID: 'Ov23linhi55EGdywrBJ8',
       clientSecret: 'afc292f4a5b4ebb30fa53ae2e3e8c86c172260d4',
-      callbackURL:'https://faxriddin.bobur-dev.uz/auth/github/callback',
-      scope: [
-        'user:email' 
-      ],
+      callbackURL: 'https://faxriddin.bobur-dev.uz/auth/github/callback',
+      scope: ['user:email'], // faqat email olish uchun yetarli
     });
   }
 
-  async validate( accessToken: string,  refreshToken: string,  profile: any,done: VerifyCallback,): Promise<any> {
-    const { name, emails, photos, _json } = profile;
-
-    let age: number | null = null;
-    if (_json && _json.birthdays && _json.birthdays.length > 0) {
-      const birthYear = _json.birthdays[0].date.year;
-      if (birthYear) {
-        const currentYear = new Date().getFullYear();
-        age = currentYear - birthYear;
-      }
-    }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { username, emails, photos } = profile;
 
     const user = {
       email: emails?.[0]?.value,
-      firstName: name?.givenName,
-      lastName: name?.familyName,
-      picture: photos?.[0]?.value, 
-      age, 
+      username,
+      picture: photos?.[0]?.value,
       accessToken,
     };
 
