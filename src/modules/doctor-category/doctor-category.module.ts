cd64d0b3d9a14bb50b205aed623e62788f4626fa
hacerovhacer <hacerovhacer253@gmail.com>
2025-10-01 16:28:12 +0500

    g

--- src/modules/doctor-category/doctor-category.module.ts
+++ src/modules/doctor-category/doctor-category.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DoctorCategoryService } from './doctor-category.service';
-// import { DoctorCategoryController } from './doctor-category.controller';
+import { DoctorCategoryController } from './doctor-category.controller';
 
 @Module({
-  // controllers: [DoctorCategoryController],
+  controllers: [DoctorCategoryController],
   providers: [DoctorCategoryService],
 })
 export class DoctorCategoryModule {}
