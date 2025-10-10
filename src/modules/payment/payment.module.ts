5559b0ac3b5b033d67344a4741e9216823d465b0
hacerovhacer <hacerovhacer253@gmail.com>
2025-10-10 22:50:50 +0500

    f

--- src/modules/payment/payment.module.ts
+++ src/modules/payment/payment.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { AuthGuard } from 'src/common/guards/jwt-auth.guard';
 
 @Module({
+  imports:[ JwtModule.registerAsync({
+    inject: [ConfigService],
+    useFactory: async (config: ConfigService) => ({
+      secret: config.get<string>('Jwt_Acc'),
+      signOptions: { expiresIn: config.get<string>('Jwt_Acc_in') },
+    }),
+  })],
   controllers: [PaymentController],
-  providers: [PaymentService],
+  providers: [PaymentService,AuthGuard],
 })
 export class PaymentModule {}
